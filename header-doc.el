1b7926baf6f11e9282fd3aada12eecd597947e59
omnitia <83828882+omnit3a@users.noreply.github.com>
2025-12-14 18:47:52 -0500

    initial commit.
    support for emacs lisp, racket, c and python

---
+++ header-doc.el
@@ -0,0 +1,34 @@
+; header-doc.el
+; personal emacs script that allows for easy creation of code file documentation
+; at the top of a file. automatically uses the correct comment syntax, depending 
+; on the language.
+; (C) 12/2025
+
+(defun header-doc-get-mode ()
+  (let ((lang-mode-string)
+	(lang-mode))
+    (setq lang-mode-string (format "%s" major-mode))
+    (car (split-string lang-mode-string "-mode"))))
+
+(defun header-doc-get-comment-style ()
+  (let ((lang-mode (header-doc-get-mode))
+	(comment-prefixes (make-hash-table :test 'equal)))
+
+    ; hashmap containing string used to start a comment
+    (puthash "emacs-lisp" ";" comment-prefixes)
+    (puthash "racket" ";" comment-prefixes)
+    (puthash "python" "#" comment-prefixes)
+    (puthash "c" "//" comment-prefixes)
+    
+    (gethash lang-mode comment-prefixes)))
+
+(defun header-doc-create ()
+  (interactive)
+  (beginning-of-buffer)
+  (let ((comment-prefix (header-doc-get-comment-style))
+	(file-name (file-relative-name (buffer-file-name))))
+    (insert (format "%s %s\n" comment-prefix file-name))
+    (insert (format "%s <insert notes here>\n" comment-prefix))
+    (insert (format "%s \n" comment-prefix))
+    (insert (format "%s \n" comment-prefix))
+    (insert (format "%s (C) %s\n\n" comment-prefix (format-time-string "%m/%Y")))))
